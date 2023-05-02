3c6a0f0c89d1f6c532a3aeacd57d5d419086106d
den <denissytnikmacho@gmail.com>
2023-05-02 18:45:23 +0300

    refactored

--- script/src/script.js
+++ script/src/script.js
@@ -1,47 +1,48 @@
-const form = document.querySelector('form');
-const textarea = document.querySelector('textarea');
-const gfButton = document.querySelector('.gf-script');
-const dmButton = document.querySelector('.dm-script');
+const form = document.querySelector("form");
+const textarea = document.querySelector("textarea");
+const btnScript1 = document.querySelector(".btn-script1");
+const btnScript2 = document.querySelector(".btn-script2");
 
-async function sendHTTPRequest(url) {
-    try {
-        const response = await fetch(url);
-        if (response.ok) return response.text();
-        throw new Error(`Request failed with status ${response.status}`);
-    } catch {
-        console.error(error);
-    }
-}
+const sendRequest = async (url) => {
+  try {
+    const response = await fetch(url);
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const params = textarea.value.trim().split('\n');
-    const queryParams = params.map(param => encodeURIComponent(param)).join(',');
-    const url = `http://127.0.0.1:17000/?cmd=${queryParams}`;
+    if (response.ok) return response.text();
 
-    sendHTTPRequest(url)
-      .then(response => console.log(response))
-      .catch(error => console.error(error));
+    throw new Error(`Request failed with status ${response.status}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const params = textarea.value.trim().split("\n");
+  const queryParams = params
+    .map((param) => encodeURIComponent(param))
+    .join(",");
+  const url = `http://127.0.0.1:17000/?cmd=${queryParams}`;
+
+  sendRequest(url);
 });
 
-gfButton.addEventListener('click', () => {
-    const url = 'http://127.0.0.1:17000/?cmd=green,bgrect%200.05%200.05%200.95%200.95,update';
+btnScript1.addEventListener("click", () => {
+  const url =
+    "http://127.0.0.1:17000/?cmd=green,bgrect%200.25%200.25%200.75%200.75,update";
 
-    sendHTTPRequest(url)
-      .then(response => console.log(response))
-      .catch(error => console.error(error));
+  sendRequest(url);
 });
 
-dmButton.addEventListener('click', () => {
-    const urlToDraw = 'http://127.0.0.1:17000/?cmd=white,figure%200.1%200.1,update';
-    const urlToMove = 'http://127.0.0.1:17000/?cmd=move%200.1%200.1,update';
+btnScript2.addEventListener("click", () => {
+  const urlToDraw =
+    "http://127.0.0.1:17000/?cmd=white,figure%200.05%200.05,update";
+  const urlToMove = "http://127.0.0.1:17000/?cmd=move%200.05%200.05,update";
 
-    sendHTTPRequest(urlToDraw);
+  sendRequest(urlToDraw);
 
-    for (let i = 0; i < 9; i++) {
-        setTimeout(() => {
-            console.log(`Request: ${i + 1}`);
-            sendHTTPRequest(urlToMove);
-        }, (i + 1) * 1000);
-    }
+  for (let i = 0; i < 21; i++) {
+    setTimeout(() => {
+      sendRequest(urlToMove);
+    }, (i + 1) * 1000);
+  }
 });
